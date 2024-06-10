547a80cc91333a2011c6a91c2abb3b022633f42c
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2024-06-10 15:49:16 +0100

    fix useCollection response id

--- src/views/playlists/CreatePlaylist.vue
+++ src/views/playlists/CreatePlaylist.vue
@@ -30,12 +30,14 @@ import useStorage from "@/composables/useStorage"
 import useCollection from "@/composables/useCollection"
 import getUser from "@/composables/getUser"
 import { timestamp } from "@/firebase/config"
+import { useRouter } from "vue-router"
 
 export default {
   setup() {
     const { filePath, url, uploadImage } = useStorage()
     const { error, addDoc } = useCollection("playlists")
     const { user } = getUser()
+    const router = useRouter()
 
     const title = ref("")
     const description = ref("")
@@ -47,19 +49,19 @@ export default {
       if (file.value) {
         isPending.value = true
         await uploadImage(file.value)
-        await addDoc({
+        const res = await addDoc({
           title: title.value,
           description: description.value,
           userId: user.value.uid,
           userName: user.value.displayName,
           coverUrl: url.value,
-          filePath: filePath.value,
+          filePath: filePath.value, // so we can delete it later
           songs: [],
           createdAt: timestamp(),
         })
         isPending.value = false
         if (!error.value) {
-          console.log("playlist added")
+          router.push({ name: "PlaylistDetails", params: { id: res.id } })
         }
       }
     }
